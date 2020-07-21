b4ea6415f8580e1731291bd3f3fa7c456105ce17
Fred <fred.gerner@icloud.com>
2020-07-21 20:25:17 +1000

    more routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 
 const app = express();
-const port = process.env || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('wez');
+});
+app.get('/weather', (req, res) => {
+    res.send('weather page');
+});
+app.get('/about', (req, res) => {
+    res.send('about page');
+});
+app.get('/help', (req, res) => {
+    res.send('Help page');
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on ${port}`)
 })
