cdbae71ff71d662549f62ba681fcf367c3fd1303
Fred <fred.gerner@icloud.com>
2020-07-22 22:37:20 +1000

    dynamic pages with handlebars

--- src/app.js
+++ src/app.js
@@ -1,19 +1,45 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('wez');
+// define paths for express
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+// setup handlebars and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+// setup static directory
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather¡',
+        name: 'Fred'
+    });
 });
-app.get('/weather', (req, res) => {
-    res.send('weather page');
-});
+
 app.get('/about', (req, res) => {
-    res.send('about page');
+    res.render('about', {
+        title: 'About'
+    });
 });
+
 app.get('/help', (req, res) => {
-    res.send('Help page');
+    res.render('help', {
+        title: 'Help',
+        message: 'run mate'
+    });
+});
+
+app.get('/weather', (req, res) => {
+    res.send({
+        temp: 40,
+        place: 'here'
+    });
 });
 
 app.listen(port, () => {
