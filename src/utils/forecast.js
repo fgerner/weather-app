0b5ce726af2677273dca81eaf02b1b1bb4b667fc
Fred <fred.gerner@icloud.com>
2020-07-28 21:58:54 +1000

    updated api variables

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('postman-request');
 
 const forecast = (lat, long, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=d90dd9462e7dd36f69783b4efff059d0&query=${lat},${long}`;
+    const url = `http://api.weatherstack.com/current?access_key=${process.env.FORECAST_KEY}&query=${lat},${long}`;
     request({url, json: true}, (error, {body} = {}) => {
         if (error) {
             callback('Unable to connect to forecast');
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('postman-request');
 
 const geocode = (address, callback) => {
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiZmdlcm5lciIsImEiOiJja2NzemcyYnMxczh1MnpvMm1icWZiOTF5In0.TxzKU6G6hZdqqjFon1m86A`;
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${process.env.GEOCODE_KEY}`;
     request({url, json: true}, (error, {body} = {}) => {
         if (error) {
             callback('Unable to connect');
