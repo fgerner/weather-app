35d1b93349488031a08f8399ab1bd64cb9c0dbde
Fred <fred.gerner@icloud.com>
2020-07-30 22:59:41 +1000

    added result to page with styling

--- public/js/app.js
+++ public/js/app.js
@@ -3,20 +3,26 @@ console.log('Client side js you know!!!');
 
 const weatherForm = document.querySelector('form');
 const searchItem = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
     let place = searchItem.value;
 
+    if(place.length === 0){
+        return messageOne.innerHTML = 'Location is mandatory';
+    }
+
+    messageOne.innerText = 'Loading...';
+
     fetch('http://localhost:3000/weather?address=' + place).then((response) => {
         response.json().then((data) => {
             if (data.err) {
-                console.log(data.err);
+                messageOne.textContent = data.err;
             } else {
-                console.log(data.location)
-                console.log(data.forecast)
-                console.log(data.temp)
-                console.log(data.feelslike)
+                messageOne.textContent = data.location;
+                messageTwo.textContent = `${data.forecast} with a temperature of ${data.temp} witch feels like ${data.feelslike}`;
             }
         })
     });
