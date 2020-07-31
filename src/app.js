06792eecadfb061da03d7757a663ef5a30d59265
Fred <fred.gerner@icloud.com>
2020-07-31 22:41:18 +1000

    updated text

--- src/app.js
+++ src/app.js
@@ -31,14 +31,14 @@ app.get('', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About',
-        name: 'bob'
+        name: 'Fred'
     });
 });
 
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help',
-        message: 'run mate',
+        message: 'Very helpful text bit...',
         name: 'Fred'
     });
 });
